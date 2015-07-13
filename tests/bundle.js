68e22c0a4dfdc1095518f673401f7dac759e9539
Daniel Silva <daniel@developmentseed.org>
2015-07-13 15:46:01 +0100

    Add tests

---
+++ tests/bundle.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+var cp = require('child_process');
+var assert = require('assert');
+var del = require('del');
+var Decompress = require('decompress');
+var collecticons = __dirname + '/../bin/collecticons.js';
+
+
+describe('testing command bundle', function() {
+  after(function() {
+    del(__dirname + '/results/test-bundle/');
+  });
+
+  it("should output zip file", function(done) {
+
+    var args = [
+      collecticons,
+      'bundle',
+      __dirname + '/fixtures/add_icon/', __dirname + '/results/test-bundle/collecticons.test.zip'
+    ];
+
+    cp.spawn('node', args, {stdio: 'inherit'})
+    .on('close', function(code) {
+      assert.equal(existsSync(__dirname + '/results/test-bundle/collecticons.test.zip'), true);
+
+      var zipContents = [
+        'icons.css',
+        'preview.html',
+        'svg/add.svg',
+        'font/collecticons.woff',
+        'font/collecticons.ttf',
+        'font/collecticons.eot',
+      ];
+
+      // unzip and check contents.
+      new Decompress({mode: '755'})
+       .src(__dirname + '/results/test-bundle/collecticons.test.zip')
+       .dest(__dirname + '/results/test-bundle')
+       .run(function() {
+
+          zipContents.forEach(function(f) {
+            assert.equal(existsSync(__dirname + '/results/test-bundle/' + f), true, 'Missing ' + f);
+          });
+
+        done();
+       });
+
+    });
+
+  });
+});
+
+function existsSync(filePath){
+  try {
+    fs.statSync(filePath);
+  }
+  catch (err) {
+    if (err.code == 'ENOENT') {
+      return false;
+    }
+    throw err;
+  }
+  return true;
+};
--- tests/grid.js
+++ tests/grid.js
@@ -1,17 +1,22 @@
 var fs = require('fs');
 var cp = require('child_process');
 var assert = require('assert');
+var del = require('del');
 var collecticons = __dirname + '/../bin/collecticons.js';
 
 describe('testing command grid', function() {
+  after(function() {
+    del(__dirname + '/results/test-grid/');
+  });
+
   it("should remove grid from svg", function(done) {
 
-    var args = [collecticons, 'grid', '-r', __dirname + '/fixtures/add_icon/', 'tests/results'];
+    var args = [collecticons, 'grid', '-r', __dirname + '/fixtures/add_icon/', __dirname + '/results/test-grid'];
 
     cp.spawn('node', args, {stdio: 'inherit'})
     .on('close', function(code) {
       assert.equal(
-        fs.readFileSync(__dirname + '/results/add.svg', {encoding: 'utf8'}),
+        fs.readFileSync(__dirname + '/results/test-grid/add.svg', {encoding: 'utf8'}),
         fs.readFileSync(__dirname + '/expected/add.svg', {encoding: 'utf8'})
       );
       done();
